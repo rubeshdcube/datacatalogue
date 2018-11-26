03f74fcd1e4f0b20d58a0b54b9bc0cb22be4323b
rubeshdcube <rubesh@d3analytics.com>
2018-11-26 21:04:04 +0530

    Update symphony.py

--- dcube/claims/symphony.py
+++ dcube/claims/symphony.py
@@ -1,5 +1,5 @@
 class symphony:
     def product_dimension_table():
-        print('Printing product_dimension_table')
+        return('Printing product_dimension_table')
     def sob_dimension_table():
-        print('Printing sob_dimension_table')	
+        return('Printing sob_dimension_table')	
