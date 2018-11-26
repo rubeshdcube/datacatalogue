a9f03d9e1d68e202520b0eafa96b569b68e0614e
rubeshdcube <rubesh@d3analytics.com>
2018-11-26 20:44:34 +0530

    create datacatalogue

---
+++ dcube/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dcube',
+          version=1.0,
+          description='Library for Symphony Data Model',
+          author='Dcube Analytics Pvt Ltd',
+          author_email='dds@d3analytics.com',
+          packages=['claims']     
+     )
+__author__ = 'Dcube Analytics Pvt Ltd'
+ 
